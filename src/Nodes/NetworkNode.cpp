73d1ae23e019c4b6b2e13f8acaf90607ac4db993
Samega7Cattac <samega7cattac@gmail.com>
2022-11-25 15:31:28 +0000

    fix: Added missing files from previous commit

---
+++ src/Nodes/NetworkNode.cpp
@@ -0,0 +1,41 @@
+#include "NetworkNode.hh"
+
+using namespace BeeCoLL;
+
+NetworkNode::NetworkNode(const NetworkNodeInfo& node_info, Coordinator* coordinator) :
+    m_info(node_info),
+    m_coordinator(coordinator)
+{
+    uint64_t unique_addr = node_info.SL;
+    unique_addr |= static_cast<uint64_t>(node_info.SH) << 32;
+    SetUniqueAddress(unique_addr);
+}
+
+NetworkNode::NetworkNode(const NetworkNode& other) :
+    m_info(other.m_info),
+    m_coordinator(other.m_coordinator)
+{
+    uint64_t unique_addr = other.m_info.SL;
+    unique_addr |= static_cast<uint64_t>(other.m_info.SH) << 32;
+    SetUniqueAddress(unique_addr);
+}
+
+NetworkNode::~NetworkNode()
+{
+
+}
+
+void
+NetworkNode::SetCallback(const std::function<void(const Frame&)>& callback)
+{
+    m_callback = callback;
+}
+
+void
+NetworkNode::TriggerCallback(const Frame& frame)
+{
+    if (m_callback)
+    {
+        m_callback(frame);
+    }
+}
---
+++ src/Nodes/NetworkNode.hh
@@ -0,0 +1,52 @@
+#ifndef BEECOLL_NODE_NETWORKNODE_HH
+#define BEECOLL_NODE_NETWORKNODE_HH
+
+// BEECOLL headers
+#include "Node.hh"
+#include "Coordinator.hh"
+
+// STD headers
+#include <functional>
+
+namespace BeeCoLL
+{
+    struct NetworkNodeInfo
+    {
+        uint16_t MY;
+        uint32_t SH;
+        uint32_t SL;
+        // TODO: DB not not he frame?????
+        uint8_t DB;
+        std::string NI;
+        uint16_t parent_network_addr;
+        uint8_t node_type;
+        uint8_t status;
+        uint16_t profile_id;
+        uint16_t manufacture_id;
+
+        // Optional
+        uint32_t digi_device_type;
+        uint32_t rssi_of_last_hop;
+    };
+
+    class NetworkNode : public Node
+    {
+    public:
+        NetworkNode(const NetworkNodeInfo& node_info,
+                    class Coordinator* coordinator);
+        NetworkNode(const NetworkNode& other);
+        ~NetworkNode();
+
+        void SetCallback(const std::function<void(const Frame&)>& callback);
+        void TriggerCallback(const Frame& frame);
+
+    private:
+        NetworkNodeInfo m_info;
+
+        class Coordinator* m_coordinator;
+
+        std::function<void(const Frame&)> m_callback;
+    };
+};
+
+#endif // BEECOLL_NODE_NETWORKNODE_HH
